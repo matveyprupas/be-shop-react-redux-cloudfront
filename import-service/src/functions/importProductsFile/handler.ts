aaec713baca36c78102c8462994557681f05641f
Matvey Prupas <matvey_prupas@epam.com>
2023-03-21 16:49:43 +0200

    fix: add signedURL

--- import-service/src/functions/importProductsFile/handler.ts
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -1,3 +1,5 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
@@ -8,9 +10,16 @@ import schema from './schema';
 
 const importProductsFile: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   const fileName = event.queryStringParameters.name;
+  const paramsUploaded = {
+    Bucket: BUCKET,
+    Key: `uploaded/${fileName}`,
+  }; 
+  const s3Client = new S3Client({region: 'eu-west-1'});
+  const putCommand = new PutObjectCommand(paramsUploaded);
+  const signedURL = await getSignedUrl(s3Client, putCommand, {expiresIn: 3600});
 
   return formatJSONResponse({
-    url: `https://${BUCKET}.s3.eu-west-1.amazonaws.com/uploaded/${fileName}`
+    url: signedURL
   });
 };
 
